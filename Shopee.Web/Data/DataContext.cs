060bd6c3dbf9644b21d19e3afdec0553b573fb92
Raúl Grados <gnr2092@gmail.com>
2019-07-30 08:41:28 -0500

    Initial Database and First Controller
    Creation of the data context, the product entity, the initial database model and the controller and views for products using code first.

---
+++ Shopee.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Shopee.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shopee.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
