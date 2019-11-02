b6a62d5c07c3c6b3aaa48452ccf74d4cc47df900
Raul Grados <gnr2092@gmail.com>
2019-11-02 12:38:15 -0500

    Create seeder for User Create default user and add it to the relationship with products.

--- Shopee.Web/Data/SeedDb.cs
+++ Shopee.Web/Data/SeedDb.cs
@@ -5,15 +5,18 @@ namespace Shopee.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity;
 
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private readonly Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
             random = new Random();
         }
 
@@ -21,23 +24,43 @@ namespace Shopee.Web.Data
         {
             await context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("jzuluaga55@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Raúl",
+                    LastName = "Gradis",
+                    Email = "grados_2008@hotmail.com",
+                    UserName = "rgrados",
+                    PhoneNumber = "+51961819297"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "mejorando");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!context.Products.Any())
             {
-                AddProduct("iPhone X");
-                AddProduct("Magic Mouse");
-                AddProduct("Keyboard Logitech");
+                AddProduct("iPhone X", user);
+                AddProduct("Magic Mouse", user);
+                AddProduct("Keyboard Logitech", user);
                 await context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             context.Products.Add(new Product
             {
                 Name = name,
                 Price = random.Next(100),
                 IsAvailabe = true,
-                Stock = random.Next(100)
+                Stock = random.Next(100),
+                User = user,
             });
         }
     }
