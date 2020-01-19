665e27967a3ec5f50330cd5e67f430103b263d32
Raul Grados <gnr2092@gmail.com>
2020-01-19 06:01:36 -0500

    Configure cascading delete and configure column price in Products

--- Shopee.Web/Data/DataContext.cs
+++ Shopee.Web/Data/DataContext.cs
@@ -3,6 +3,7 @@
     using Microsoft.EntityFrameworkCore;
     using Entities;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using System.Linq;
 
     public class DataContext : IdentityDbContext<User>
     {
@@ -12,6 +13,25 @@
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            var cascadeFKs = modelBuilder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
